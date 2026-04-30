30d677e763566e88030153e36e2e59b7434a4762
Алексей Морозов <CoBaLtKaMi@yandex.ru>
2026-04-30 14:01:51 +0300

    Добавление CI пайплайна.

--- SportClubSystem/SportClubApi.Tests/UnitTest1.cs
+++ SportClubSystem/SportClubApi.Tests/UnitTest1.cs
@@ -1,11 +1,8 @@
-namespace SportClubApi.Tests
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void BasicTest_Passes()
     {
-        [Fact]
-        public void Test1()
-        {
-
-        }
+        Assert.Equal(2, 1 + 1);
     }
 }
